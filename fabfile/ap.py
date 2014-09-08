87d54121777d0c60b96b19d3cbdf5ddbe1ed1327
Christopher Groskopf <staringmonkey@gmail.com>
2014-09-08 14:37:38 -0700

    Simplify ap requests.

--- fabfile/ap.py
+++ fabfile/ap.py
@@ -9,8 +9,9 @@ import app_config
 
 SECRETS = app_config.get_secrets()
 
-def _get_races(use_cache):
-    url = 'https://api.ap.org/v2/init/races/2014-11-04?officeID=S,H,G&format=json'
+def _get_ap(endpoint, params={}, use_cache=True):
+    url = 'https://api.ap.org/v2/init/%s/2014-11-04' % endpoint
+    
     cache = {}
     headers = {}
 
@@ -18,24 +19,33 @@ def _get_races(use_cache):
         with open('_ap_cache.json') as f:
             cache = json.load(f)
 
-        url = cache['races']['nextrequest']
-        headers['If-Modified-Since'] = cache['races']['Last-Modified']
-        headers['If-None-Match'] = cache['races']['Etag']
+        url = cache[endpoint]['nextrequest']
+        headers['If-Modified-Since'] = cache[endpoint]['Last-Modified']
+        headers['If-None-Match'] = cache[endpoint]['Etag']
 
-    url += '&apiKey=%s' % SECRETS['AP_API_KEY']
-    response = requests.get(url, headers=headers)
+        # If using cache, other params have already been added to url
+        params = {
+            'apiKey': SECRETS['AP_API_KEY']
+        }
+    else:
+        params.update({
+            'format': 'json',
+            'apiKey': SECRETS['AP_API_KEY']
+        })
+
+    response = requests.get(url, params=params, headers=headers)
 
     if response.status_code == 304:
-        print 'Races already up to date'
+        print '%s: already up to date' % endpoint
         return
     elif response.status_code == 403:
-        print 'Rate-limited'
+        print '%s: rate-limited' % endpoint
         return
     elif response.status_code != 200:
-        print 'Error retrieving races: %i' % response.status_code
+        print '%s: returned %i' % (endpoint, response.status_code)
         return
 
-    cache['races'] = {
+    cache[endpoint] = {
         'nextrequest': response.json()['nextrequest'],
         'Last-Modified': response.headers['Last-Modified'],
         'Etag': response.headers['Etag']
@@ -44,13 +54,13 @@ def _get_races(use_cache):
     with open('_ap_cache.json', 'w') as f:
         json.dump(cache, f)
 
-    print 'Updated races'
+    print '%s: updated' % endpoint
 
 @task
 def bootstrap():
-    _get_races(False)
+    _get_ap('races', { 'officeID': 'S,H,G' }, False)
 
 @task
 def update():
-    _get_races(True)
+    _get_ap('races', { 'officeID': 'S,H,G' }, True)
      
