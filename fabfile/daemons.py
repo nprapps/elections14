0c929794dd4ef63c2f17b9e8520e6d2ff14df62d
Tyler Fisher <tyler@tylerjfisher.com>
2014-09-23 17:19:49 -0400

    make slide rotation work on staging

--- fabfile/daemons.py
+++ fabfile/daemons.py
@@ -4,15 +4,17 @@ from time import sleep
 
 from fabric.api import execute, task
 
+import app_config
+
 @task
 def tumblr():
     while True:
         execute('tumblr.get_posts')
         execute('deploy_slides')
-        sleep(30)
+        sleep(app_config.TUMBLR_REFRESH_INTERVAL)
 
 @task
 def rotate_slide():
     while True:
         execute('stack.rotate')
-        sleep(15)
+        sleep(app_config.SLIDE_ROTATE_INTERVAL)
