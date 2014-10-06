657e420792474775cf14ccbe6d7a4cd7399ede08
Tyler Fisher <tyler@tylerjfisher.com>
2014-10-06 13:28:52 -0400

    chosen > jquery

--- www/js/stack.js
+++ www/js/stack.js
@@ -1,14 +1,3 @@
-var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-];
-
 var $welcomeScreen = null;
 var $welcomeButton = null;
 
@@ -99,6 +88,9 @@ function getStack() {
 var onWelcomeButtonClick = function() {
     $welcomeScreen.hide();
     $statePickerScreen.show();
+
+    $('.state-selector').chosen({max_selected_options: 1});
+
 }
 
 var onStatePickerSubmit = function(e) {
@@ -127,30 +119,6 @@ var setUpAudio = function() {
     });
 }
 
-var substringMatcher = function(strs) {
-    return function findMatches(q, cb) {
-        var matches, substrRegex;
-
-        // an array that will be populated with substring matches
-        matches = [];
-
-        // regex used to determine if a string contains the substring `q`
-        substrRegex = new RegExp(q, 'i');
-
-        // iterate through the pool of strings and for any string that
-        // contains the substring `q`, add it to the `matches` array
-        $.each(strs, function(i, str) {
-          if (substrRegex.test(str)) {
-            // the typeahead jQuery plugin expects suggestions to a
-            // JavaScript object, refer to typeahead docs for more info
-            matches.push({ value: str });
-          }
-        });
-    cb(matches);
-    };
-};
-
-
 $(document).ready(function() {
     $welcomeScreen = $('.welcome');
     $welcomeButton = $('.welcome-button')
@@ -170,15 +138,4 @@ $(document).ready(function() {
     $statePickerForm.submit(onStatePickerSubmit);
 
     setUpAudio();
-
-    $('.state-search .typeahead').typeahead({
-        hint: true,
-        highlight: true,
-        minLength: 1
-    },
-    {
-        name: 'states',
-        displayKey: 'value',
-        source: substringMatcher(states)
-    });
 });
