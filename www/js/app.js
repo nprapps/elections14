38986d384ebb3975c251495f6a4d924140a17b86
Tyler Fisher <tyler@tylerjfisher.com>
2014-10-09 13:17:33 -0400

    remove chosen and implement typeahead

--- www/js/app.js
+++ www/js/app.js
@@ -30,6 +30,18 @@ var mouseMoveTimer = null;
 
 var firstShareLoad = true;
 
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+
 /*
  * Run on page load.
  */
@@ -75,7 +87,6 @@ var onDocumentReady = function(e) {
     clippy.on('ready', function(readyEvent) {
         clippy.on('aftercopy', onClippyCopy);
     });
-
     // Geolocate
     geoip2.city(onLocateIP);
 
@@ -108,16 +119,42 @@ var onWelcomeButtonClick = function() {
     $statePickerScreen.show();
     resizeSlide($statePickerScreen);
 
-    $('.state-selector').chosen({max_selected_options: 1});
-
-    $('.state-selector').on('change', function(evt, params) {
-        if (params['selected']) {
-            var abbrev = params['selected'].toLowerCase();
-            $stateface.removeClass();
-            $stateface.addClass('stateface stateface-' + abbrev);
-        }
+    $('.typeahead').typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+    },
+    {
+        name: 'states',
+        displayKey: 'value',
+        source: substringMatcher(states)
     });
+
 }
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+
+    cb(matches);
+  };
+};
 
 /*
  * Fullscreen the app.
@@ -182,7 +219,11 @@ var offControlsHover = function() {
 var onStatePickerSubmit = function(e) {
     e.preventDefault();
 
-    state = $('.state-selector').val();
+    var input = $('.tt-input').val();
+    var inverted = _.invert(APP_CONFIG.STATES);
+    console.log(inverted);
+    state = inverted[input];
+    console.log(state);
 
     if (!(state)) {
         alert("Please pick a state!");
