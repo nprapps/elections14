b8731e74a985b6880ca2f75de3e4626604781819
Tyler Fisher <tyler@tylerjfisher.com>
2014-10-14 16:41:51 -0400

    refactor state autocomplete function to only match beginnings of strings. closes #547

--- www/js/app.js
+++ www/js/app.js
@@ -228,27 +228,23 @@ var onWelcomeButtonClick = function() {
 }
 
 var substringMatcher = function(strs) {
-  return function findMatches(q, cb) {
-    var matches, substrRegex;
+    return function findMatches(q, cb) {
+        var matches = [];
 
-    // an array that will be populated with substring matches
-    matches = [];
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function(i, str) {
+            var queryLength = q.length;
+            var normalizedString = str.toLowerCase();
+            if (normalizedString.substring(0, queryLength) === q.toLowerCase()) {
+                // the typeahead jQuery plugin expects suggestions to a
+                // JavaScript object, refer to typeahead docs for more info
+                matches.push({ value: str });
+            }
+        });
 
-    // regex used to determine if a string contains the substring `q`
-    substrRegex = new RegExp(q, 'i');
-
-    // iterate through the pool of strings and for any string that
-    // contains the substring `q`, add it to the `matches` array
-    $.each(strs, function(i, str) {
-      if (substrRegex.test(str)) {
-        // the typeahead jQuery plugin expects suggestions to a
-        // JavaScript object, refer to typeahead docs for more info
-        matches.push({ value: str });
-      }
-    });
-
-    cb(matches);
-  };
+        cb(matches);
+    };
 };
 
 /*
