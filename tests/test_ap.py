8b033b7e386ac1b99ac052345e79ba40fae0b12c
David Eads <davideads@gmail.com>
2014-10-21 20:04:30 -0400

    look for keys and clean up AP ftp tests

--- tests/test_ap.py
+++ tests/test_ap.py
@@ -4,18 +4,24 @@ import json
 import os
 import unittest
 
+from app_config import STATES
 from fabfile import ap
+
+NUM_RACES = 540
+NUM_SENATE_RACES = 36
+NUM_HOUSE_RACES = 435
+NUM_GOV_RACES = 36
+NUM_CANDIDATES = 1529
 
 class APTestCase(unittest.TestCase):
     """
     Tests for the AP scraper.
     """
 
-    def test_init(self):
+    def test_init_races(self):
         """
         Verify race data is properly written to disk
         """
-
         ap.init('.tests')
 
         with open('.tests/init_races.json') as f:
@@ -24,12 +30,78 @@ class APTestCase(unittest.TestCase):
             house_races = [race for race in races if race['office_id'] == 'H']
             gov_races = [race for race in races if race['office_id'] == 'G']
 
-            self.assertEqual(len(races), 540)
-            self.assertEqual(len(senate_races), 36)
-            self.assertEqual(len(house_races), 435)
-            self.assertEqual(len(gov_races), 36)
+            # Correct number of races initialized, fixed #s like Senate Races are
+            # particularly useful for testing.
+            self.assertEqual(NUM_RACES, len(races))
+            self.assertEqual(NUM_SENATE_RACES, len(senate_races))
+            self.assertEqual(NUM_HOUSE_RACES, len(house_races))
+            self.assertEqual(NUM_GOV_RACES, len(gov_races))
 
+            # Are keys correct?
+            expected_keys = [
+                'office_name',
+                'seat_name',
+                'seat_number',
+                'race_type',
+                'office_id',
+                'state_postal',
+                'race_id',
+            ]
+            self.assertItemsEqual(races[0], expected_keys)
+
+            # Are there weird or missing states?
+            for race in races:
+                self.assertIn(race.get('state_postal'), STATES.keys())
+
+    def test_init_candidates(self):
+        """
+        Verify candidate data is properly written to disk
+        """
+        ap.init('.tests')
 
         with open('.tests/init_candidates.json') as f:
             candidates = json.load(f)
-            self.assertEqual(len(candidates), 1529)
+
+            # Correct number of candidates
+            self.assertEqual(len(candidates), NUM_CANDIDATES)
+
+            # Correct keys
+            expected_keys = [
+                'first_name',
+                'last_name',
+                'incumbent',
+                'candidate_id',
+                'race_id',
+                'party',
+            ]
+            self.assertItemsEqual(candidates[0], expected_keys)
+
+    def test_update(self):
+        """
+        Test writing updates to disk
+        """
+        ap.update('.tests')
+
+        with open('.tests/update.json') as f:
+            races = json.load(f)
+
+            # Correct number of races updated
+            self.assertEqual(NUM_RACES, len(races))
+
+            # Keys are correct
+            expected_keys = [
+                'candidates',
+                'race_id',
+                'precincts_reporting',
+                'precincts_total',
+            ]
+            self.assertItemsEqual(expected_keys, races[0].keys())
+
+
+    def test_calls(self):
+        """
+        Test writing calls to disk
+        """
+
+        # @TODO what's the right test here?
+        pass
