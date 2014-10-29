602b4d207627fff38d14f181959084e1575fd38e
Christopher Groskopf <staringmonkey@gmail.com>
2014-10-29 17:25:37 -0400

    Deregister resize event for BOP graphic when slide is unloaded. Closes #364.

--- www/js/app.js
+++ www/js/app.js
@@ -54,7 +54,6 @@ var STATES = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
  * Run on page load.
  */
 var onDocumentReady = function(e) {
-
     // Cache jQuery references
     $welcomeScreen = $('.welcome');
     $welcomeButton = $('.welcome-button')
