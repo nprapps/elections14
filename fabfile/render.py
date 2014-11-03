80d6029f5553aa9670d0e48377ef819f3b6564ce
David Eads <davideads@gmail.com>
2014-11-03 16:02:00 -0500

    remove print statement from render results again

--- fabfile/render.py
+++ fabfile/render.py
@@ -206,7 +206,7 @@ def _render_results_slide(slug, output_path):
             path = url_for(view_name, slug=slug)
 
         with app.app.test_request_context(path=path):
-            print 'Rendering %s' % path
+            #print 'Rendering %s' % path
 
             view = app.__dict__[view_name]
             content = view(slug)
